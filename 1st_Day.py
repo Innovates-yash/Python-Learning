738abc5925a18e78f429412866e72d88d06cbf23
Innovates-yash <gyash6991@gmail.com>
2025-12-13 01:40:04 +0530

    Learning Start

---
+++ 1st_Day.py
@@ -0,0 +1,8 @@
+# Todays Learning topic are : 
+# 1. comments in python
+# 2. variables in python
+# 3. Naming conventions in python
+# 4. Data types in python
+
+a = 45/5
+print(type(a))  # This will print the data type of variable 'a'
