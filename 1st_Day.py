10deda284ffecd82f77b505800aae5783d5d7dae
Innovates-yash <gyash6991@gmail.com>
2025-12-13 02:15:02 +0530

    1st day end

--- 1st_Day.py
+++ 1st_Day.py
@@ -6,3 +6,16 @@
 
 a = 45/5
 print(type(a))  # This will print the data type of variable 'a'
+# float data are number that are decimal point values or write p/q form
+# b = 45//5
+
+# complex  data type 
+B = 3 + 5j
+print(type(B))  # This will print the data type of variable 'B' of  complex type
+
+# String data type
+c = "Hello, Python!"
+
+# String Indexing and slicing
+print(c[0])      # prints 'H'
+print(c[0:5])    # prints 'Hello'
